b3be78197c638b0b8253caba09e96973f7a2a8d5
agusev <audetv@gmail.com>
2023-08-17 18:02:09 +0300

    В сообщение добавлена структура ReplyMarkup

--- consumer/internal/domain/message/message.go
+++ consumer/internal/domain/message/message.go
@@ -1,7 +1,17 @@
 package message
 
 type Message struct {
-	ChatID    string `json:"chat_id"`
-	Text      string `json:"text"`
-	ParseMode string `json:"parse_mode"`
+	ChatID      string      `json:"chat_id"`
+	Text        string      `json:"text"`
+	ParseMode   string      `json:"parse_mode,omitempty"`
+	ReplyMarkup ReplyMarkup `json:"reply_markup,omitempty"`
+}
+
+type ReplyMarkup struct {
+	InlineKeyboard [][]InlineButton `json:"inline_keyboard,omitempty"`
+}
+
+type InlineButton struct {
+	Text string `json:"text"`
+	URL  string `json:"url"`
 }
