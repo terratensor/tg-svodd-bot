8da8361ddf9ec010e64c269f3c7e9f22de0a45da
Aleksey Gusev <audetv@gmail.com>
2025-01-05 07:45:59 +0300

    Попытка быстрой правки размера чанка

--- consumer/internal/infra/msgparser/msgparser.go
+++ consumer/internal/infra/msgparser/msgparser.go
@@ -64,7 +64,7 @@ func (p *Parser) Parse(msg string) ([]string, error) {
 	}
 	f(n)
 
-	messages := splitMessage(strings.TrimSpace(builder.String()), 4096)
+	messages := splitMessage(strings.TrimSpace(builder.String()), 4000)
 
 	return messages, nil
 }
