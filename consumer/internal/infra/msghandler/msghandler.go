d60802eab24f37436dfee0ceb1eae3fb202ea9a4
agusev <audetv@gmail.com>
2025-03-17 14:28:29 +0300

    feat(spam): Установлено ограничение на публикацию короткиих сообщений

--- consumer/internal/infra/msghandler/msghandler.go
+++ consumer/internal/infra/msghandler/msghandler.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"regexp"
 	"sync"
 	"tg-svodd-bot/consumer/internal/infra/msgparser"
 	"tg-svodd-bot/consumer/internal/infra/msgsender"
@@ -40,6 +41,23 @@ func Handler(ctx context.Context, ch chan Request, wg *sync.WaitGroup, tgmessage
 				sentry.CaptureMessage(fmt.Sprint(err))
 				log.Printf("error: %v Text: %s", err, r.Message)
 			}
+
+			// Если сообщение одно, проверяем его длину без учета цитирования
+			if len(messages) == 1 {
+				message := messages[0]
+				// Удаляем цитирование, обрамленное тегами <i></i>
+				re := regexp.MustCompile(`<i>.*?</i>`)
+				cleanedMessage := re.ReplaceAllString(message, "")
+
+				// Проверяем длину сообщения
+				if len(cleanedMessage) < 216 {
+					// Помечаем сообщение как заблокированное
+					m.MessagesBlocked.WithLabelValues().Inc()
+					log.Printf("Message blocked due to insufficient length: %s", message)
+					continue
+				}
+			}
+
 			// Отправляем подготовленные сообщения в телеграм
 			msgsender.Send(ctx, messages, r.Headers, tgmessages, m)
 		}
